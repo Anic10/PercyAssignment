8f283c690bd4e05fa70ac0e03a0ef47c63272fb0
Aniket Khaire <anic10@Anikets-MacBook-Pro.local>
2021-12-03 15:13:57 +0530

    Changes made to PERCY_BRANCH

--- cypress/integration/testspec.js
+++ cypress/integration/testspec.js
@@ -1,5 +1,4 @@
 describe('Integration test with visual testing', function() {
-
   context('Running in resolution', () => {
     beforeEach(() => {
       cy.viewport(1280, 720)
@@ -12,7 +11,8 @@ describe('Integration test with visual testing', function() {
 
       //Verifying the content
       cy.get('h1').should('have.text', 'App & Browser Testing Made Easy')
-      
+      cy.wait(1000);
+
       // Take a snapshot for visual diffing
       cy.percySnapshot('Homepage' ,{ widths: [375, 480, 720, 1280, 1440, 1920] })
     });
@@ -24,7 +24,8 @@ describe('Integration test with visual testing', function() {
 
       //Verifying the content
       cy.get('.plans-wrapper > :nth-child(1) > .live-element').should('have.text', 'Replace your device lab and VMs with any of these plans')
-      
+      cy.wait(1000);
+
       // Take a snapshot for visual diffing
       cy.percySnapshot('Amount page',{ widths: [375, 480, 720, 1280, 1440, 1920] })
     });
@@ -36,6 +37,7 @@ describe('Integration test with visual testing', function() {
 
       //Verifying the content
       cy.get('h1').should('have.text', 'Integrations that make life easier')
+      cy.wait(1000);
 
       // Take a snapshot for visual diffing
       cy.percySnapshot('App page',{ widths: [375, 480, 720, 1280, 1440, 1920] })
@@ -48,6 +50,7 @@ describe('Integration test with visual testing', function() {
 
       //Verifying the content
       cy.get('h1').should('have.text', 'Documentation')
+      cy.wait(1000);
 
       // Take a snapshot for visual diffing
       cy.percySnapshot('Help page'),{ widths: [375, 480, 720, 1280, 1440, 1920] }
